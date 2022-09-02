98e366323d0cc5a352ca8cfaf811924d7ae727fe
carlos molina <carlos.jmr90@gmail.com>
2022-09-02 16:48:30 -0300

    Ejercicio 3 arreglado

--- js/break.js
+++ js/break.js
@@ -1,11 +1,5 @@
 //Este archivo debe calcular el factorial de 10 utilizando un bucle while, una bifurcación if y una sentencia break
 
-// let numero = 10
-// let resultado = 1;
-// for (let i = 1; i <= numero; i++){
-//     resultado = resultado * i
-// }
-// alert('El factorial de 10 es '+resultado);
 
 let numero = 10;
 let resultado = 1;
